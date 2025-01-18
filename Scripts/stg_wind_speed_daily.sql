8b64e7a0f370549f1bddf22002932d6935376407
Dhawal Panchal <55388587+dhawalpanchal1997@users.noreply.github.com>
2025-01-18 17:58:45 -0600

    Update stg_wind_speed_daily.sql

--- Scripts/stg_wind_speed_daily.sql
+++ Scripts/stg_wind_speed_daily.sql
@@ -1,1 +1,13 @@
 
+WITH source AS (
+    SELECT * FROM `epa_historical_air_quality.wind_daily_summary`
+    WHERE state_name = 'Washington'
+    AND city_name = 'Seattle'
+)
+SELECT 
+    date_local as date,
+    arithmetic_mean as wind_speed,
+    state_name,
+    city_name
+FROM source
+WHERE arithmetic_mean IS NOT NULL;
