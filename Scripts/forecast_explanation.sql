b06245d609c8e7f6a6d32595ffaa173e3596c62b
Dhawal Panchal <55388587+dhawalpanchal1997@users.noreply.github.com>
2025-01-18 18:17:21 -0600

    Update forecast_explanation.sql

--- Scripts/forecast_explanation.sql
+++ Scripts/forecast_explanation.sql
@@ -1,5 +1,17 @@
 SELECT
   *
 FROM
-  ML.EXPLAIN_FORECAST(MODEL `bqml_tutorial.seattle_pm25_xreg_model`,
-    STRUCT(30 AS horizon, 0.8 AS confidence_level));
+  ML.EXPLAIN_FORECAST(
+    MODEL `bqml_tutorial.seattle_pm25_xreg_model`,
+    STRUCT(30 AS horizon, 0.8 AS confidence_level),
+    (
+      SELECT 
+        date,
+        temperature,
+        winf_speed
+     FROM
+        `seattle_air_qualit_daily`
+     WHERE 
+        date > DATE('2020-12-31')
+    )
+ );
