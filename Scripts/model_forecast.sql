1fe38e723c29fe5cc3c11632e3cc8ca36b62b32d
Dhawal Panchal <55388587+dhawalpanchal1997@users.noreply.github.com>
2025-01-18 18:18:55 -0600

    Update model_forecast.sql

--- Scripts/model_forecast.sql
+++ Scripts/model_forecast.sql
@@ -2,6 +2,17 @@ SELECT
   *
 FROM
   ML.FORECAST(MODEL `bqml_tutorial.seattle_pm25_xreg_model`,
-    STRUCT(30 AS horizon, 0.8 AS confidence_level))
+    STRUCT(30 AS horizon, 0.8 AS confidence_level),
+    (
+         SELECT 
+           date,
+           temperature,
+           winf_speed
+        FROM
+           `seattle_air_qualit_daily`
+        WHERE 
+           date > DATE('2020-12-31')
+       )
+ )
 ORDER BY
   forecast_timestamp;
